b38500e8c7c0cb628d2020eb1f5c9956fb926ca9
Raj Kumar <INT113@xceedance.com>
2019-03-27 18:01:38 +0530

    adding exhage function

--- jspart2/currency/main.js
+++ jspart2/currency/main.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded',() =>{
     document.querySelector("#form").onsubmit = () => {
 const currency =document.querySelector("#currency").value;
-fetch('https://api.exchangeratesapi.io/latest?base=USD&symbols')
-    }
+console.log(currency);
+fetch(`https://api.exchangeratesapi.io/latest?base=USD&symbols=${currency}`).then(
+    response => response.json()).then(
+        data => {
+            const contents = `1 USD is equal to ${data.rates[currency]} ${currency}.`;
+            document.querySelector("#result").innerHTML=contents;
+
+        }).catch( () =>{
+            document.querySelector("#result").innerHTML="there was an Error!";
+        });
+return false;
+    };
+    
 });
