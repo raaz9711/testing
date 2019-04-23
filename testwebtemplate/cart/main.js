e64fd5246207f05e7ef4aa67676570e89633307d
Raj Kumar <INT113@xceedance.com>
2019-04-23 11:34:12 +0530

    adding popup

--- testwebtemplate/cart/main.js
+++ testwebtemplate/cart/main.js
@@ -1,10 +1,22 @@
 var jumbotron =document.querySelector(".jumbotron");
 
 var addcart=document.querySelector(".addcart");
+var cross=document.querySelector(".cross");
+var cross1=document.querySelector(".cross1");
+
 
 var feedback=document.querySelector(".feedback");
+var popup=document.querySelector(".popup");
 addcart.addEventListener("click",myfunc);
 feedback.addEventListener("click",myfunc1);
+cross.addEventListener("click",() => {
+    popup.style.display="none";
+});
+cross1.addEventListener("click",() => {
+    popup.style.display="none";
+});
+
+
 
 function myfunc(){
     // document.body.style.opacity="0.2";
@@ -27,3 +39,8 @@ else{
 }
 jumbotron.style.display="none";
 }
+
+let tiktikboom= setTimeout(() =>{
+    console.log("y have 5 seconds");
+    popup.style.display="block";
+},2000);
